cef5d3d25109a556b0e21636d3747b2e389ddbd1
derickson <David.Erickson@gmail.com>
2015-11-08 13:00:02 -0500

    working on logstash 2.0

---
+++ logstash20/load.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+head -n 3 "../CapitalbikeshareData/Source CapitalBikeshare Data/2010-Q4-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+
+
+
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2010-Q4-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+
+
+#  cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2011-Q1-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+#  cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2011-Q2-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+#  cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2011-Q3-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2011-Q4-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2012-Q1-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2012-Q2-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2012-Q3-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2012-Q4-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2013-Q1-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2013-Q2-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2013-Q3-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat1.conf
+
+
+
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2013-Q4-Trips-History-Data2.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat2.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2014-Q1-Trips-History-Data2.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat2.conf
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2014-Q2-Trips-History-Data2.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat2.conf
+
+
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2014-Q3-Trips-History-Data3.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat3.conf
+
+# cat "../CapitalbikeshareData/Source CapitalBikeshare Data/2014-Q4-Trips-History-Data.csv" | sed -e "1d" | /Users/dave/dev/logstash/logstash-2.0.0/bin/logstash -w 4 --pluginpath ../. -f bikeshareFormat4.conf
